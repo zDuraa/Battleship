b63ab5f4d303bc3a4991cda59321f1713d11af64
zDuraa <sahinduramaz@gmail.com>
2024-11-27 12:01:01 +0100

    Printing an Integer Board

--- Battleship/Main.c
+++ Battleship/Main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 
+#define BOARDLENGTH 10
+
+void vPrintBoard(int iaBoard[][BOARDLENGTH]);
 
 int main() {
+    int iaPlayerA[BOARDLENGTH][BOARDLENGTH] = {
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+    };
 
-	printf("Hello World, this is Main");
-	return 0;
+    vPrintBoard(iaPlayerA);
+    return 0;
+}
+
+
+//prints board
+void vPrintBoard(int iaBoard[][BOARDLENGTH]) {
+    char caLetters[BOARDLENGTH] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+
+    // Kopfzeile (Zahlen von 0 bis 9)
+    printf("  ");
+    for (int i = 0; i < BOARDLENGTH; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    // Board-Zeilen mit Buchstaben und Werten
+    for (int iX = 0; iX < BOARDLENGTH; iX++) {
+        printf("%c ", caLetters[iX]);
+        for (int iY = 0; iY < BOARDLENGTH; iY++) {
+            printf("%d ", iaBoard[iX][iY]);
+        }
+        printf("\n");
+    }
 }
